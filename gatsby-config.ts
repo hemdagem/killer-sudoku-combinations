ef5063e8a0680e1419d0c991b0d476dcaf79b4e6
Hemang Rajyaguru <hemdagem@gmail.com>
2023-05-10 10:43:21 +0100

    added app install

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,6 +11,18 @@ const config: GatsbyConfig = {
       "icon": "src/images/icon.png"
     }
   },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Killer Sudoku Combinations`,
+      short_name: `KSC`,
+      start_url: `/`,
+      background_color: `#f7f0eb`,
+      theme_color: `#a2466c`,
+      display: `standalone`,
+      icon: `src/images/icon.png`
+    },
+  },
   'gatsby-plugin-offline',
   `gatsby-transformer-json`,
   {
